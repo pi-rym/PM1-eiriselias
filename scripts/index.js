760e171be9d605ec8ef15b884ca51899d3165147
eiriselias <eiriselias@gmail.com>
2024-03-21 07:35:28 -0500

    se ordenaron algunas lineas segun peticion de la actividad

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,19 @@
 class Activity{
-    constructor(id, title, description, url){
+    constructor(id, title, description, imgUrl){
         this.id = id;
         this.title = title;
         this.description = description;
-        this.url = url;
+        this.imgUrl = imgUrl;
     }
 }
-
 class Repository{
     constructor(){
         this.activities = [];
         this.id = 1;
     }
     //metodo que resiba datos de una actividad, cree una actividad
-    createActivity(title, description, url){
-        const newActivity = new Activity(this.id, title, description, url);    
+    createActivity(title, description, imgUrl){
+        const newActivity = new Activity(this.id, title, description, imgUrl);    
         this.id++;    
         this.activities.push(newActivity);
         alert("Se agrego la actividad "+title)
@@ -22,7 +21,7 @@ class Repository{
     maquetado(articulo){
         return `<article data-id="${articulo.id}" class="articulo">
         <img
-          src="${articulo.url}"
+          src="${articulo.imgUrl}"
           onerror="this.src='./assets/img/sinImagen.png';"
         />
         <div>
@@ -58,6 +57,25 @@ class Repository{
 
 const repositorio = new Repository();
 
+function handler(e){
+    e.preventDefault();
+        const title = document.querySelector("#title").value;
+        const description = document.querySelector("#description").value;
+        const imgUrl = document.querySelector("#url").value;
+
+        if(title=="" || description=="" || imgUrl==""){
+            return alert("debe llenar los todos campos");
+        }
+
+        repositorio.createActivity(title, description, imgUrl);
+        repositorio.getAllActivities();
+
+        //despues de agregar la tarjeta me hace scroll a la seccion de ellas
+        const seccionTarjetas = document.getElementById("seccionTarjetas");
+        seccionTarjetas.scrollIntoView({ behavior: 'smooth' });
+
+        document.getElementById("formulario").reset();
+}
 
 //recargo el dom
 document.addEventListener("DOMContentLoaded", function(){
@@ -71,20 +89,7 @@ document.addEventListener("DOMContentLoaded", function(){
     });
 
     const formulario = document.querySelector("#formulario");
-    formulario.addEventListener("submit", (e)=>{
-        e.preventDefault();
-        const title = document.querySelector("#title").value;
-        const description = document.querySelector("#description").value;
-        const url = document.querySelector("#url").value;
-        repositorio.createActivity(title, description, url);
-        repositorio.getAllActivities();
-
-        //despues de agregar la tarjeta me hace scroll a la seccion de ellas
-        const seccionTarjetas = document.getElementById("seccionTarjetas");
-        seccionTarjetas.scrollIntoView({ behavior: 'smooth' });
-
-        document.getElementById("formulario").reset();
-    })
+    formulario.addEventListener("submit", handler)
 
     repositorio.getAllActivities();
 });
